277744481c3351734aa833167483d5dce598fe4b
Tyuiu.SorokinAD@outlook.com <abuzius@mail.ru>
2023-10-19 06:47:10 +0500

    Add project files.

---
+++ Tyuiu.SorokinAD.Sprint3.Task0.V22.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.SorokinAD.Sprint3.Task0.V22.Lib;
+
+namespace Tyuiu.SorokinAD.Sprint3.Task0.V22.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double value = 6;
+            int startValue = 1;
+            int stopValue = 2;
+
+            double res = ds.GetMultiplySeries(value, startValue, stopValue);
+            double wait = 48.009;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.SorokinAD.Sprint3.Task0.V22/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.SorokinAD.Sprint3.Task0.V22.Lib;
+
+namespace Tyuiu.SorokinAD.Sprint3.Task0.V22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3| Выполнил: Сорокин А. Д. | ИИПб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Оператор цикла for                                                *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #22                                                             *");
+            Console.WriteLine("* Выполнил Сорокин А. Д. | ИИПб-23-2                                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу используя цикл for, которая вычисляет                *");
+            Console.WriteLine("* произведение ряда по формуле, при a = 0,25                              *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double value = 0.25;
+            int startValue = 1;
+            int stopValue = 8;
+            Console.WriteLine($"Переменная a = {value}");
+            Console.WriteLine($"Старт шага = {startValue}");
+            Console.WriteLine($"Конец шага = {stopValue}");
+   
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Резльтат                                                                *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine($"Сумма рада = {ds.GetMultiplySeries(value, startValue, stopValue)}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
